9ab2f4c5f67ef362d8a46fdd168c212925a61956
Vitor Hugo <Vitor Hugo>
2018-05-15 19:27:10 -0300

    QuestaoDAO - nao finalizado

--- src/main/java/model/Questao.java
+++ src/main/java/model/Questao.java
@@ -1,13 +1,33 @@
 package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="questao")
 public class Questao {
 
 	private int codigo;
 	private String enunciado;
 	private String nivel;
-	private String[] assertivas = new String[5];
+	private String gabarito;
+	private String assertivaA;
+	private String assertivaB;
+	private String assertivaC;
+	private String assertivaD;
+	private String assertivaE;
+	private int codAssunto;
 
-	public void cadastrar(String enunciado, String nivel, String[] assertivas, int codAssunto) {
-
+	public void cadastrar(int codigo, String enunciado, String nivel, String gabarito, String[] assertivas, int codAssunto) {
+		this.codigo = codigo;
+		this.enunciado = enunciado;
+		this.nivel = nivel;
+		this.gabarito = gabarito;
+		this.assertivaA = assertivas[0];
+		this.assertivaB = assertivas[1];
+		this.assertivaC = assertivas[2];
+		this.assertivaD = assertivas[3];
+		this.assertivaE = assertivas[4];
+		this.codAssunto = codAssunto;
 	}
 
 	public void atualizar(String enunciado, String nivel, String[] assertivas, int codAssunto) {
@@ -17,7 +37,9 @@ public class Questao {
 	public void remover(int codigo) {
 
 	}
-
+	
+	@Id
+	@Column(name="codigo")
 	public int getCodigo() {
 		return codigo;
 	}
@@ -42,14 +64,58 @@ public class Questao {
 		this.nivel = nivel;
 	}
 
-	public String[] getAssertivas() {
-		return assertivas;
+	@Column(name="assertiva_a")
+	public String getAssertivaA() {
+		return assertivaA;
 	}
 
-	public void setAssertivas(String[] assertivas) {
-		this.assertivas = assertivas;
+	public void setAssertivaA(String assertivaA) {
+		this.assertivaA = assertivaA;
+	}
+
+	@Column(name="assertiva_c")
+	public String getAssertivaB() {
+		return assertivaB;
+	}
+
+	public void setAssertivaB(String assertivaB) {
+		this.assertivaB = assertivaB;
 	}
 	
-	
+	@Column(name="assertiva_c")
+	public String getAssertivaC() {
+		return assertivaC;
+	}
+
+	public void setAssertivaC(String assertivaC) {
+		this.assertivaC = assertivaC;
+	}
+
+	@Column(name="assertiva_d")
+	public String getAssertivaD() {
+		return assertivaD;
+	}
+
+	public void setAssertivaD(String assertivaD) {
+		this.assertivaD = assertivaD;
+	}
+
+	@Column(name="assertiva_e")
+	public String getAssertivaE() {
+		return assertivaE;
+	}
+
+	public void setAssertivaE(String assertivaE) {
+		this.assertivaE = assertivaE;
+	}
+
+	@Column(name="cod_assunto")
+	public int getCodAssunto() {
+		return codAssunto;
+	}
+
+	public void setCodAssunto(int codAssunto) {
+		this.codAssunto = codAssunto;
+	}
 
 }
---
+++ src/main/java/persistencia/QuestaoDAO.java
@@ -0,0 +1,109 @@
+package persistencia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+
+import model.Questao;
+
+
+public class QuestaoDAO extends Manager{
+ 
+    protected void create(Questao q) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(q);
+        session.getTransaction().commit();
+        session.close();
+    }
+    
+    public List<Questao> getAll(){     
+    	
+    	List<Questao> q;
+    	
+    	Session session = sessionFactory.openSession();
+    	
+        Query professor = session.createQuery("from Questao");
+        q = professor.getResultList();
+        session.close();
+        return q;
+	}
+ 
+    protected void read() {
+    	Session session = sessionFactory.openSession();
+	    session.close();
+    }
+ 
+    protected void update(Questao q) {
+        // code to modify a questao
+        q.setCodigo(q.getCodigo());
+        q.setEnunciado(q.getEnunciado());
+        q.setNivel(q.getNivel());
+        q.setAssertivaA(q.getAssertivaA());
+        q.setAssertivaB(q.getAssertivaB());
+        q.setAssertivaC(q.getAssertivaC());
+        q.setAssertivaD(q.getAssertivaD());
+        q.setAssertivaE(q.getAssertivaE());
+     
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(q);
+        session.getTransaction().commit();
+        session.close();
+    	 
+    }
+ 
+    protected void delete(Questao q) {
+    	Session session = sessionFactory.openSession();
+	    session.beginTransaction();
+	 
+	    session.delete(q);
+	 
+	    session.getTransaction().commit();
+	    session.close();
+    }
+    
+    protected void delete(int codigo) {
+        // code to remove a book	
+    	Questao q = new Questao();
+	    q.setCodigo(codigo);
+	 
+	    Session session = sessionFactory.openSession();
+	    session.beginTransaction();
+	 
+	    session.delete(q);
+	 
+	    session.getTransaction().commit();
+	    session.close();
+    	
+    }
+    
+    public static void main(String[] args) {
+        // code to run the program
+    	
+    	String[] assertivas = {"Bral", "Brel", "Bril", "Brol", "Brão"};
+    	
+    	Questao q = new Questao();
+    	q.cadastrar(1234, "Quem descobriu o Brasil ? Pedro Álvares Ca...", "Ensino Médio", "Assertiva A", assertivas, 1);
+    	
+    	String[] assertivas2 = {"Bral", "Brel", "Bril", "Brol", "Brão"};
+    	
+    	Questao q2 = new Questao();
+    	q2.cadastrar(4321, "Quem é o presidente do Brasil ?", "Ensino Médio", "Assertiva D", assertivas2, 2);
+    	
+    	QuestaoDAO bm = new QuestaoDAO();
+    	bm.setup();
+    	bm.create(q);
+    	bm.create(q2);
+    	bm.exit();
+    	
+    
+        for (Object p : bm.getAll()){
+        	System.out.println(((Questao)q).getEnunciado());
+        }
+        
+        bm.exit();
+    }
+}
